24a29bf11ff49a49163bb0f6dd902087e5b1fba7
Nikita <nikita2015borisov@gmail.com>
2023-04-30 00:08:16 +0300

    Information lab 2

---
+++ Information/Задания для заочников/Задания для заочников/Lab2/Lab2/Lab2/Program.cs
@@ -0,0 +1,92 @@
+﻿
+
+class Program
+{
+    // Шестой вариант
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Выберите способ решения задания, введите '1' или '2':");
+
+
+        double solveVariant = getIntVarFromInput();
+
+        switch (solveVariant)
+        {
+            case 1:
+                solveTask1();
+                break;
+            case 2:
+                solveTask2(2);
+                break;
+            default:
+                Console.WriteLine("Такого варианта не существует");
+                break;
+        }
+
+        //Console.WriteLine("Task result: " + solveTask(a, b, c));
+    }
+
+    private static void solveTask1()
+    {
+        Console.WriteLine("Введите числа:");
+        double a = getDoubleVarFromInput();
+
+        int count = 0;
+        if (a >= 0)
+        {
+            count++;
+        }
+
+        a = getDoubleVarFromInput();
+        if (a >= 0)
+        {
+            count++;
+        }
+
+        Console.WriteLine("Колличество неотрицательных чисел равно: " + count);
+    }
+
+    private static void solveTask2(int countNumbers)
+    {
+        Console.WriteLine("Введите числа:");
+        // заполняем массив с числами
+        int count = 0;
+        for (int i = 1; i <= countNumbers; i++)
+        {
+            double a = getDoubleVarFromInput();
+
+            if (a >= 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Колличество неотрицательных чисел равно: " + count);
+    }
+
+    private static double getDoubleVarFromInput()
+    {
+        double a;
+        if (!Double.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Your variable is not double type");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        return a;
+    }
+
+    private static double getIntVarFromInput()
+    {
+        int a;
+        if (!Int32.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Your variable is not int type");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        return a;
+    }
+}
