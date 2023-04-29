c071642635bc6a705ce96aff19e6fe54a9419183
Nikita <nikita2015borisov@gmail.com>
2023-04-29 23:30:00 +0300

    Information lab 1

---
+++ Information/Задания для заочников/Задания для заочников/Lab1/Lab1/Lab1/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Input 3 double numbers:");
+        double a = getVarFromInput();
+        double b = getVarFromInput();
+        double c = getVarFromInput();
+
+        Console.WriteLine("Task result: " + solveTask(a, b, c));
+    }
+
+    // Шестой вариант
+    private static double solveTask(double a, double b, double c)
+    {
+        double firstStep = a - 2 * Math.Pow(Math.Sin(b), 3);
+        double secondStep = a * (Math.Tan(c) + b);
+        return firstStep / secondStep;
+    }
+
+    private static double getVarFromInput()
+    {
+        double a;
+        if (!Double.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Your variable is not double type");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+
+        return a;
+    }
+}
